a67dc72dd0981e565f2c092e4eab0ae77faa8ea0
Jim Totland <jimtotland@gmail.com>
2022-03-02 14:07:14 +0100

    coded some more on 1c), waiting for minas response

--- project2/problem1.R
+++ project2/problem1.R
@@ -44,3 +44,33 @@ L.pines <- Kfn(ppinit("pines.dat"), fs = 10, k = 100)
 L.pines.df <- data.frame(x = L.pines$x, y = L.pines$y)
 ggplot(L.pines.df) + geom_line(aes(x, y)) + geom_abline(intercept = 0, slope = 1, color = "red") + 
   theme_minimal()
+
+## c) ----
+
+sim.ppp <- function(xmin, xmax, ymin, ymax, n){
+  N <- 100 # Number of simulations
+  k <- 100 # Number of grid points
+  L.mat <- matrix(rep(NA, N*k), nrow = N)
+  for(i in 1:100){
+    x.sim <- runif(n, xmin, xmax)
+    y.sim <- runif(n, ymin, ymax)
+    L <- Kfn(list(x = x.sim, y = y.sim), fs = 0.5, k = k)
+    print(length(L$x))
+    #print(L$k)
+    L.mat[i, ] = L$y
+  }
+  return(list(x = L$x, y = L.mat))
+}
+
+n.cells <- length(cells$x)
+cells.pois <- sim.ppp(0,1,0,1, n.cells)
+mean <- apply(cells.pois$y, 2, mean)
+S2 <- apply(cells.pois$y, 2, var)
+z.005 <- qnorm(0.05, lower.tail = FALSE)
+lower <- mean  - z.005*sqrt(S2)
+upper <- mean  + z.005*sqrt(S2)
+
+ggplot(data = data.frame(l = lower, u = upper, x = cells.pois$x)) + 
+  geom_ribbon(aes(x = x, ymin = l, ymax = u))
+
+plot(cells.pois$x, cells.pois$y[, ])
