160da4c08b59821a35010a1bb1ceda334babdbde
Jim Totland <jimtotland@gmail.com>
2022-04-23 17:09:07 +0200

    added code for d)

--- project3/problem2.R
+++ project3/problem2.R
@@ -2,6 +2,11 @@ setwd("~/Github/romstat/project3")
 source("resources/functions.R")
 load("resources/Admin1Geography.RData")
 
+# Inverse of logit
+expit <- function(x){
+  return(exp(x)/(1+exp(x)))
+}
+
 v1 <- read.table("resources/Admin1Graph.txt")
 d1 <- as.matrix(v1) %*% as.vector(rep(1, 37))
 R1 <- diag(as.numeric(d1)) - unlist(v1)
@@ -11,11 +16,6 @@ V <- direct.est.df$StdDev^2
 y <- direct.est.df$Observation
 
 ## c) ----
-
-# Inverse of logit
-expit <- function(x){
-  return(exp(x)/(1+exp(x)))
-}
 
 # Sample from multivariate normal with precision matrix
 sample.mvn <- function(n, mu, prec){
@@ -46,3 +46,37 @@ P.sd <- apply(P.cond, 2, sd)
 P.mean <- apply(P.cond, 2, mean)
 P.CV <- P.sd/P.mean # Cofficient of variation
 plotAreaCol("figures/P_CV.pdf", 20, 20, P.CV, nigeriaAdm1, "Coefficient of Variation")
+
+## d) ----
+y38 <- 0.5
+y.tilde <- c(y, y38)
+V.tilde <- c(V, 0.1^2)
+Q1 <- R1
+Q2 <- diag(1/V.tilde)
+M <- matrix(0, nrow = 38, ncol = 37)
+M[col(M) == row(M)] = 1
+M[38, 19] = 1 # Kaduna is area number 19
+
+mu <- solve(Q1 + t(M)%*%Q2%*%M) %*% t(M) %*% Q2 %*% y.tilde
+Q <- Q1 + t(M)%*%Q2%*%M
+
+sim.P <- function(n, Q, mu){
+  P.mat <- matrix(NA, nrow = n, ncol = 37)
+  x.var <- 1/diag(Q)
+  for(i in 1:n){
+    x <- rnorm(37, mu, sqrt(x.var))
+    P.mat[i, ] <- expit(x)
+  }
+  return(P.mat)
+}
+
+# Find and plot Median
+P.mat <- sim.P(100, Q, mu)
+P.median <- apply(P.mat, 2, median)
+plotAreaCol("figures/P_median_d.pdf", 20, 20, P.median, nigeriaAdm1, "Median")
+
+# Find and plot CV
+P.sd <- apply(P.mat, 2, sd)
+P.mean <- apply(P.mat, 2, mean)
+P.CV <- P.sd/P.mean # Cofficient of variation
+plotAreaCol("figures/P_CV_d.pdf", 20, 20, P.CV, nigeriaAdm1, "Coefficient\nof Variation")
